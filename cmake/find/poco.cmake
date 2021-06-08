40f1a8ca99bcc4cd511c7bf111f97473cea94031
msaf1980 <msaf1980@gmail.com>
2021-06-08 08:10:30 +0500

    graphite: add rules lookup benchmark (temp fix for find poco)

--- cmake/find/poco.cmake
+++ cmake/find/poco.cmake
@@ -1,6 +1,14 @@
 option (USE_INTERNAL_POCO_LIBRARY "Use internal Poco library" ON)
 
-if (NOT USE_INTERNAL_POCO_LIBRARY)
+if (USE_INTERNAL_POCO_LIBRARY)
+    set (
+        POCO_INCLUDE_DIR
+        "${ClickHouse_SOURCE_DIR}/contrib/poco/Foundation/include"
+        "${ClickHouse_SOURCE_DIR}/contrib/poco/Util/include"
+        "${ClickHouse_SOURCE_DIR}/contrib/poco/XML/include"
+        "${ClickHouse_SOURCE_DIR}/contrib/poco/Net/include"
+    )
+else()
     find_path (ROOT_DIR NAMES Foundation/include/Poco/Poco.h include/Poco/Poco.h)
     if (NOT ROOT_DIR)
         message (${RECONFIGURE_MESSAGE_LEVEL} "Can't find system poco")
