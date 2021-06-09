b0bd647853bfe6f75e506d188d282d02640cf0d4
msaf1980 <msaf1980@gmail.com>
2021-06-09 17:14:00 +0500

    graphite: tests for selectPatternForPath

---
+++ src/Processors/Merges/Algorithms/tests/gtest_graphite.cpp
@@ -0,0 +1,128 @@
+#include <fstream>
+#include <cstring>
+#include <filesystem>
+#include <stdexcept>
+
+namespace fs = std::filesystem;
+
+#include <gtest/gtest.h>
+
+#include <Common/tests/gtest_global_context.h>
+#include <Common/tests/gtest_global_register.h>
+
+#include <Common/Config/ConfigProcessor.h>
+#include <AggregateFunctions/registerAggregateFunctions.h>
+#include <Processors/Merges/Algorithms/Graphite.h>
+
+using namespace DB;
+
+static int regAggregateFunctions = 0;
+
+void tryRegisterAggregateFunctions() {
+    if (!regAggregateFunctions) {
+        registerAggregateFunctions();
+        regAggregateFunctions = 1;
+    }
+}
+
+const auto dir = fs::path(__FILE__).parent_path();
+
+static ConfigProcessor::LoadedConfig loadConfiguration(const std::string& config_path) {
+    ConfigProcessor config_processor(config_path, true, true);
+    ConfigProcessor::LoadedConfig config = config_processor.loadConfig(false);
+    return config;
+}
+
+static Graphite::Params setGraphitePatternsFromConfigFile(const std::string& config_path) {
+    auto config = loadConfiguration(config_path);
+
+    Graphite::Params params;
+    setGraphitePatternsFromConfig(*config.configuration.get(), "graphite_rollup", params);
+
+    return params;
+}
+
+static fs::path join(const fs::path& root_dir, const std::string& rel_path) {
+    fs::path j_path = root_dir;
+    j_path.append(rel_path);
+    return j_path;
+}
+
+typedef struct {
+    std::string path;
+    size_t retention_index;
+    size_t aggregation_index;
+} patterns_for_path;
+
+
+static std::vector<patterns_for_path> loadPatternsforPath(const std::string &metrics_file) {
+    std::vector<patterns_for_path> metrics;
+
+    std::string str;
+    std::ifstream in_stream;
+    in_stream.open(metrics_file);
+    if (! in_stream.is_open())
+        throw std::runtime_error(metrics_file + " error: " + std::strerror(errno));
+
+    while (1)
+    {
+        patterns_for_path p;
+        in_stream >> p.path;
+        if (in_stream.eof())
+            break;
+        if (in_stream.fail())
+            throw std::runtime_error(metrics_file + " error: " + std::strerror(errno));
+        in_stream >> p.retention_index;
+        if (in_stream.eof())
+            throw std::runtime_error(metrics_file + " error: unexpected eof");
+        if (in_stream.fail())
+            throw std::runtime_error(metrics_file + " error: " + std::strerror(errno));
+        in_stream >> p.aggregation_index;
+        if (in_stream.eof())
+            throw std::runtime_error(metrics_file + " error: unexpected eof");
+        if (in_stream.fail())
+            throw std::runtime_error(metrics_file + " error: " + std::strerror(errno));
+
+        metrics.push_back(p);
+    }
+
+    return metrics;
+}
+
+TEST(GraphiteTest, testSelectPattern)
+{
+    tryRegisterAggregateFunctions();
+
+    auto config_path = join(dir, "rollup.xml");
+    auto metrics_path = join(dir, "rollup.txt");
+    Graphite::Params params = setGraphitePatternsFromConfigFile(config_path.string());
+
+    std::vector<patterns_for_path> tests = loadPatternsforPath(metrics_path.string());
+
+    for (const auto & t : tests) {
+        auto rule = DB::Graphite::selectPatternForPath(params, t.path);
+        auto retention_want = params.patterns[t.retention_index];
+        auto aggregation_want = params.patterns[t.aggregation_index];
+        EXPECT_EQ(*rule.first, retention_want) << "retention rollup rule mismatch for " << t.path;
+        EXPECT_EQ(*rule.second, aggregation_want) << "aggregation rollup mismatch rule for " << t.path;
+    }
+}
+
+TEST(GraphiteTest, testSelectPatternTyped)
+{
+    tryRegisterAggregateFunctions();
+
+    auto config_path = join(dir, "rollup-typed.xml");
+    auto metrics_path = join(dir, "rollup-typed.txt");
+    Graphite::Params params = setGraphitePatternsFromConfigFile(config_path.string());
+
+    std::vector<patterns_for_path> tests = loadPatternsforPath(metrics_path.string());
+
+    for (const auto & t : tests) {
+        auto rule = DB::Graphite::selectPatternForPath(params, t.path);
+        auto retention_want = params.patterns[t.retention_index];
+        auto aggregation_want = params.patterns[t.aggregation_index];
+        EXPECT_EQ(*rule.first, retention_want) << "retention typed rollup rule mismatch for " << t.path;
+        EXPECT_EQ(*rule.second, aggregation_want) << "aggregation typed rollup mismatch rule for " << t.path;
+    }
+}
